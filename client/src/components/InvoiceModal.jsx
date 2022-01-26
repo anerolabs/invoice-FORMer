945484b483632eb50dd4dde3595185298dc30702
Andrew Carnero <andrew.carnero@gmail.com>
2022-01-26 15:22:43 -0500

    Update order details section of invoice modal to render all receipt items. Add basic CSS styling

--- client/src/components/InvoiceModal.jsx
+++ client/src/components/InvoiceModal.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const InvoiceModal = ({expandInvoice, invoiceData, savedProfile}) => {
+  let dateArray = invoiceData.orderDate.split('T');
+  let date = dateArray[0];
+  let time = dateArray[1].slice(0, 5);
+
+  const receipt = JSON.parse(invoiceData.receipt);
+
   console.log(invoiceData);
   console.log(savedProfile);
+  console.log(receipt);
 
   return ( <div className='modal-fill'>
       <div className='modal-content'>
@@ -34,7 +41,7 @@ const InvoiceModal = ({expandInvoice, invoiceData, savedProfile}) => {
             <span className='i-h2'>Customer Name:</span>
             <span className='i-v'>{invoiceData.email}</span>
             <span className='i-h2'>Order Placed:</span>
-            <span className='i-v'>{invoiceData.date}</span>
+            <span className='i-v'>{date} {time}</span>
           </div>
         </div>
 
@@ -46,17 +53,36 @@ const InvoiceModal = ({expandInvoice, invoiceData, savedProfile}) => {
             Order Details
           </div>
           <div className='i-details'>
-            Receipt details here
+            <table className='m-receipt'>
+              <thead>
+              <tr>
+                <th></th>
+                <th>Item Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+              </thead>
+              <tbody>
+              {Object.keys(receipt).map((product, i) => {
+                return <tr key={i}>
+                  <td>{i + 1}.</td>
+                  <td>{product}</td>
+                  <td>{receipt[product][0]}</td>
+                  <td className='m-r-p'>{'$'}{receipt[product][1].toFixed(2)}</td>
+                </tr>})}
+              </tbody>
+            </table>
+
           </div>
           <div className='i-total'>
             <span className='i-t-s'>
-              Subtotal: {invoiceData.subTotal}
+              Subtotal: ${invoiceData.subTotal.toFixed(2)}
             </span>
             <span className='i-t-x'>
-              Tax: TAX HERE
+              Tax: $TAX HERE
             </span>
             <span className='i-t-t'>
-              Total: {invoiceData.subTotal}
+              Total: ${invoiceData.subTotal.toFixed(2)}
             </span>
           </div>
         </div>
