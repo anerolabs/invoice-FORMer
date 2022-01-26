4248f710f5c251ab14de3cdbc92609b4a3b2c5eb
Andrew Carnero <andrew.carnero@gmail.com>
2022-01-26 13:24:59 -0500

    Added HTML structure to the invoice modal, moved img directory to the dist directory

--- client/src/components/InvoiceModal.jsx
+++ client/src/components/InvoiceModal.jsx
@@ -7,22 +7,43 @@ const InvoiceModal = ({expandInvoice, invoiceData, savedProfile}) => {
   return ( <div className='modal-fill'
     onClick={() => {expandInvoice()}}>
       <div className='modal-content'>
-      <h1>Order Summary</h1>
-      <h3>Thank you for your order!</h3>
-      Customer: First Last
-      email@email.com
 
-      Order Placed: 1/1/22
+        <div className='modal-header'>
+          <div className='1'>
+            <img src='./img/wingsandrings.jpg' />
+          </div>
+          <div className='2'>
+            <span className='i-title'>Order Summary</span>
+            <span className='i-h2'>Customer Name:</span>
+            <span className='i-v'>{invoiceData.first} {invoiceData.last}</span>
+            <span className='i-h2'>Customer Name:</span>
+            <span className='i-v'>{invoiceData.email}</span>
+            <span className='i-h2'>Order Placed:</span>
+            <span className='i-v'>{invoiceData.date}</span>
+          </div>
+        </div>
 
-      This is the message on the invoice!
+        <div className='modal-body'>
+          <div className='i-msg'>
+            {savedProfile.invoiceMessage}
+          </div>
+          <div className='i-h1'>
+            Order Details
+          </div>
+          <div className='i-details'>
+            Receipt details here
+          </div>
+          <div className='i-total'>
+            Subtotal: {invoiceData.subTotal}
+            Tax: TAX HERE
+            Total: {invoiceData.subTotal}
+          </div>
+        </div>
 
-      ORDER DETAILS
-      Item ..... QTY ..... PRICE
-      Subtotal:
-      Tax:
-      Order Total:
+        <div className='modal-footer'>
+          {savedProfile.bName} | {savedProfile.bWebsite} | {savedProfile.bPhone}
+        </div>
 
-      Business Name | Business Website | Business Phone
       </div>
   </div>
   )
